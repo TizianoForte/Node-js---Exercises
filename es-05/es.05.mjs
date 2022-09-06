5c6b9bed30ec0b4f09ce2303e27f403b3a0dfaf1
Tiziano Forte <forte.tiziano@gmail.com>
2022-09-06 13:44:51 +0200

    05 exercise

---
+++ es-05/es.05.mjs
@@ -0,0 +1,7 @@
+function sum (x,y) {
+    const sum = x+y;
+    console.log(sum)
+    return `the sum is: ${sum}`;  
+}
+
+export default sum;
---
+++ es-05/script.mjs
@@ -0,0 +1,3 @@
+import sum from "./es.05.mjs";
+
+sum(5,6);
