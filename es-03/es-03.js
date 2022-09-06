f5e6dfaf8cf7e4cd6fe9b36672d30913aa4eadfc
Tiziano Forte <forte.tiziano@gmail.com>
2022-09-06 13:35:19 +0200

    03 exercises

---
+++ es-03/es-03.js
@@ -0,0 +1,8 @@
+function sum (x,y) {
+    const sum = x+y;
+    console.log(sum)
+    return `the sum is: ${sum}`;  
+}
+
+
+module.exports = sum;
---
+++ es-03/script.js
@@ -0,0 +1,3 @@
+const sum = require("./es-03")
+
+sum(5,6);
